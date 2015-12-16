7cecb59bc9dcb4fea0bda7aa2f69ef73dbe13ffc
Cao Xiaoqi <xiaoqi.cao@rea-group.com>
2015-12-16 23:43:44 +0800

    hw3, Add ParkingManager. Xiaoqi

--- homework3/caoxiaoqi/src/main/java/com/tw/xqcao/ParkingManager.java
+++ homework3/caoxiaoqi/src/main/java/com/tw/xqcao/ParkingManager.java
@@ -1,5 +1,31 @@
 package com.tw.xqcao;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ParkingManager {
-    
+
+    private final List<ParkingAgent> parkingAgents;
+
+    public ParkingManager(ParkingAgent... parkingAgentList) {
+        parkingAgents = Arrays.asList(parkingAgentList);
+    }
+
+    public String parkCar(Car car) {
+        for (ParkingAgent parkingAgent : parkingAgents) {
+            String token = parkingAgent.parkCar(car);
+            if (token != null)
+                return token;
+        }
+        return null;
+    }
+
+    public Car pickCar(String token) {
+        for (ParkingAgent parkingAgent : parkingAgents) {
+            Car car = parkingAgent.pickCar(token);
+            if (car != null)
+                return car;
+        }
+        return null;
+    }
 }
--- homework3/caoxiaoqi/src/test/java/com/tw/xqcao/ParkingManagerTest.java
+++ homework3/caoxiaoqi/src/test/java/com/tw/xqcao/ParkingManagerTest.java
@@ -1,9 +1,42 @@
 package com.tw.xqcao;
 
+import com.tw.xqcao.finder.CommonFinder;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 
 public class ParkingManagerTest {
 
+    private ParkingManager parkingManager;
+
+    @Before
+    public void setUp() {
+        Parkinglot parkinglot = new Parkinglot("aa", 2);
+        ParkingAgent parkingAgent1 = new ParkingAgent(new CommonFinder());
+        parkingAgent1.addParkinglot(parkinglot);
+
+        ParkingAgent parkingAgent2 = new ParkingAgent(new CommonFinder());
+        parkingAgent1.addParkinglot(parkinglot);
+
+        parkingManager = new ParkingManager(parkingAgent1, parkingAgent2);
+    }
+
+    @Test
+    public void shouldParkOneCar() {
+        Car car = new Car("SA 00001");
+        String token = parkingManager.parkCar(car);
+
+        assertThat(token, notNullValue());
+    }
+
+    @Test
+    public void shouldPickOneCar() {
+        Car car = new Car("SA 00001");
+        String token = parkingManager.parkCar(car);
+
+        assertThat(parkingManager.pickCar(token), is(car));
+    }
 }
